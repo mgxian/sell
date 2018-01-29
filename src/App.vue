20fa92c015f3425c73bc1f13f119dcf9ca2ee130
will <maoguangxian@famulei.com>
2018-01-29 22:27:06 +0800

    border-1px

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <v-header></v-header>
-    <div class="tab">
+    <div class="tab border-1px">
       <div class="tab-item">
         <router-link to="/goods">商品</router-link>
       </div>
@@ -28,13 +28,21 @@ export default {
 </script>
 
 <style lang="stylus">
+@import "./common/stylus/mixin.styl"
 #app
   .tab
     display flex
     width 100%
     height 40px
     line-height 40px
+    border-1px(rgba(7, 17, 27, 0.1))
     .tab-item
       flex 1
       text-align center
+      & > a
+        display block
+        font-size 14px
+        color rgb(77, 85, 93)
+        &.router-link-active
+          color rgb(240, 20, 20)
 </style>
